62efb7160953d603be2c9a6bd9150d1221111c2b
Sdqumar <sdqumar09@gmail.com>
2021-12-01 14:53:31 +0100

    test

--- pages/api/check.js
+++ pages/api/check.js
@@ -1,18 +1,39 @@
 
 const  puppeteer = require('puppeteer');
+let finalResult
 
 const handler = async (req, res) => {
   // const { number } = req.query;
   const number = "eeb72z"
   const url ='https://my.service.nsw.gov.au/MyServiceNSW/index#/rms/freeRegoCheck/details';
   try{
-      let finalResult="dsds"
-   const browser = await puppeteer.launch({ headless: false});
+   const browser = await puppeteer.launch({ headless:true});
    const page = await browser.newPage();
 
-   await page.goto(url);
-await browser.close()
    
+   await page.goto(url);
+   await page.waitForSelector('#formly_2_input_plateNumber_0');
+   await page.type('#formly_2_input_plateNumber_0', number);
+   await page.click(
+     '#formly_2_checkbox-label-with-action_termsAndConditions_1'
+   );
+   page.on('response', async (response) => {
+     response.text().then(function (textBody) {
+       if (textBody.startsWith('[')) {
+         const json = JSON.parse(textBody);
+         const body = json[0];
+         if (body.method == 'postVehicleListForFreeRegoCheck') {
+           finalResult = body.result;
+           browser.close();
+         }
+       }
+     });
+   });
+   await Promise.all([
+     page.click('button[type="submit"]'),
+     page.waitForNavigation({ waitUntil: 'networkidle0' }),
+   ]);
+
 
     // console.log('doneee');
     if (finalResult) {
