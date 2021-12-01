0fd79069161abaf4492aedfbce9dce8166b6c3da
Sdqumar <sdqumar09@gmail.com>
2021-12-01 19:12:01 +0100

    good

--- pages/api/check.js
+++ pages/api/check.js
@@ -1,39 +1,38 @@
 
-const  chrome = require('chrome-aws-lambda');
+const chromium = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
 
 const handler = async (req, res) => {
-  const { number } = req.query;
-  const url =
-    'https://my.service.nsw.gov.au/MyServiceNSW/index#/rms/freeRegoCheck/details';
-  let browser;
-  let finalResult = '';
-  try {
-      browser = await puppeteer.launch({
-        args: chrome.args, 
-        executablePath: await chrome.executablePath,
-        headless: true,
-      })
-    const page = await browser.newPage();
 
-    await page.goto(url);
-     finalResult = page.title();
-            browser.close();
-      
-    await Promise.all([
-      page.click('button[type="submit"]'),
-      page.waitForNavigation({ waitUntil: 'networkidle0' }),
-    ]);
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath,
+    headless: true,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto('https://spacejelly.dev/');
+
+  await page.focus('#search-query')
+  await page.keyboard.type('api');
+
+  const results = await page.$$eval('#search-query + div a', (links) => {
+    return links.map(link => {
+      return {
+        text: link.innerText,
+        href: link.href
+      }
+    });
+  });
+
+  await browser.close();
 
     // console.log('doneee');
-    if (finalResult) {
       res.status(200).json({
-        data: finalResult,
-      });
-    }
-  } catch (error) {
-    res.status(200).json({ data: finalResult });
-  }
+        data: results,
+    })
+  
 };
 
 export default handler;
