37f36db95e8a5baa8b41bb64ccaa4612a98b33ab
Sdqumar <sdqumar09@gmail.com>
2021-12-01 21:44:10 +0100

    final

--- pages/api/check.js
+++ pages/api/check.js
@@ -1,38 +1,57 @@
-
-const chromium = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+const chrome = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
 
 const handler = async (req, res) => {
+  const { number } = req.query;
 
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath: process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath,
-    headless: true,
-  });
-
-  const page = await browser.newPage();
-
-  await page.goto('https://spacejelly.dev/');
-
-  await page.focus('#search-query')
-  await page.keyboard.type('api');
-
-  const results = await page.$$eval('#search-query + div a', (links) => {
-    return links.map(link => {
-      return {
-        text: link.innerText,
-        href: link.href
-      }
+  const url =
+    "https://my.service.nsw.gov.au/MyServiceNSW/index#/rms/freeRegoCheck/details";
+  let finalResult = "";
+  try {
+    
+    //change process.env.CHROME_EXECUTABLE_PATH to your chrome executablePath to run locally. go to chrome address bar and type chrome://version
+    const browser = await puppeteer.launch({
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath:
+        process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath),
+      headless: true,
+      ignoreHTTPSErrors: true,
     });
-  });
 
-  await browser.close();
+    const page = await browser.newPage();
 
-    // console.log('doneee');
+    await page.goto(url);
+    await page.waitForSelector("#formly_2_input_plateNumber_0");
+    await page.type("#formly_2_input_plateNumber_0", number);
+    await page.click(
+      "#formly_2_checkbox-label-with-action_termsAndConditions_1"
+    );
+    page.on("response", async (response) => {
+      response.text().then(function (textBody) {
+        if (textBody.startsWith("[")) {
+          const json = JSON.parse(textBody);
+          const body = json[0];
+          if (body.method == "postVehicleListForFreeRegoCheck") {
+            finalResult = body.result;
+            browser.close();
+          }
+        }
+      });
+    });
+    await Promise.all([
+      page.click('button[type="submit"]'),
+      page.waitForNavigation({ waitUntil: "networkidle0" }),
+    ]);
+
+    if (finalResult) {
       res.status(200).json({
-        data: results,
-    })
-  
+        data: finalResult,
+      });
+    }
+  } catch (error) {
+    res.status(200).json({ data: finalResult });
+  }
 };
 
 export default handler;
